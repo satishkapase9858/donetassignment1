e98751c7ea1099786ce2b3a4beb6b94f5e7b269e
satishkapase9858 <65151853+satishkapase9858@users.noreply.github.com>
2020-12-02 19:13:46 +0530

    assignment2
    assignment2

--- Program.cs
+++ Program.cs
@@ -4,111 +4,130 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Assignment1
+namespace assignmentQ2
 {
-    class Program
+    class   Program
     {
         static void Main(string[] args)
         {
-            employee p1 = new employee();
-           
-            p1.Name ="sss";
-            p1.Deptno = 9;
-            p1.Basic = 15;
-            Console.WriteLine(p1.EmpNo);
-            Console.WriteLine(p1.Name);
-            Console.WriteLine(p1.Basic);
-           
-
-
-               employee p2 = new employee();
-                Console.WriteLine(p2.EmpNo);
-                
-
-
-
-            employee p3 = new employee();
-            Console.WriteLine(p3.EmpNo);
-
-
-
-            employee e1 = new employee("satish", 12212, 10);
-            employee e2 = new employee("satish", 12212);
-            employee e3 = new employee("satish");
-            Console.WriteLine(e1.getnetsalary());
+            //Employee e1 = new Manager();
+            //e1.Basic = 101;
+            //Console.WriteLine("basic value of employee"+e1.Basic);
+            //Console.ReadLine();
+            //Console.WriteLine(e1.CalcNetSalary());
+            //Console.ReadLine();
+            Employee e2 = new CEO();
+            Console.WriteLine(  e2.CalcNetSalary());
             Console.ReadLine();
-
         }
     }
 
 
-
-    class employee
+ public  abstract class  Employee
     {
-
-
-
         private String name;
-        public String Name
+          public String Name
         {
             set
             {
-                if (value=="")
-                {
-                    Console.WriteLine("invallid entry in String name");
-                    
-                }
+                if (value == "")
+                    Console.WriteLine("invalid name");
                 else
-                {
-
                     name = value;
-                }
-
 
             }
-
             get
             {
-
                 return name;
             }
 
         }
 
-        private static int dd =1;
-          
-
-        private  int empno;
-           public   int EmpNo
+        private int empno;
+          public int EmpNo
         {
-             
-              get
+            get
             {
-                return  empno;
-
-
+                return empno;
             }
 
         }
-          
-       
 
-
-        private decimal basic;
-        public decimal Basic
+        private short deptno;
+        public short DeptNo
         {
-            set { 
-            if(value>10 && value< 100)
+            set
             {
-                    basic = value;
+                if (value > 0)
+                    deptno = value;
+                else
+                    Console.WriteLine("invalid entry in deptno");
+
             }
+            get
+            {
+                return deptno;
+            }
+
+        }
+
+        public abstract decimal Basic
+        {
+            get;
+            set;
+        }
+
+        public abstract decimal CalcNetSalary();
+         public  Employee()
+        {
+            Console.WriteLine("constructor for abstract employee class");
+
+        }
+        public Employee(String name, int empno,short deptno )
+        {
+            Console.WriteLine("constructor for abstract employee class");
+            this.name = name;
+            this.empno = empno;
+            this.deptno = deptno;
+              
+        }
+
+    }
+ public class Manager : Employee
+    {    
+
+        private String designation;
+        public String Designation
+        {
+            set
+            {
+                if (value == "")
+                    Console.WriteLine("invalid name");
+                else
+                    designation = value;
+
+            }
+            get
+            {
+                return designation;
+            }
+
+        }
+         
+        private decimal basic;
+        public override decimal Basic
+        {
+            set
+            {
+                if (value > 100)
+                {
+                    basic = value;
+                }
                 else
                 {
-
-                    Console.WriteLine("not allowed values in decimal part");
+                    Console.WriteLine("invalid entry of manager basic");
                 }
-
-           }
+            }
             get
             {
 
@@ -116,56 +135,103 @@ namespace Assignment1
             }
         }
 
-        private short deptno;
-             public  short Deptno
+
+        public Manager()
+        {
+            Console.WriteLine("  no paramer constructor for abstract overridden employee class with manager");
+            this.designation = "satish";
+            this.basic = 101;
+             
+        }
+
+        public Manager(String designation, int basic)
+        {
+            Console.WriteLine("this is manager class  with  parameter cnstructor");
+            this.basic = basic;
+            this.designation = designation;
+
+        }
+        public override decimal CalcNetSalary()
+        {
+            return basic + 1000;
+        }
+ 
+    }
+ public class GeneralManager:Manager
+    {
+        private String perk;
+          public string Perk
         {
             set
             {
-                if(value>0)
+                perk = value;
+            }
+            get
+            {
+                return perk;
+
+            }
+
+        }
+          public GeneralManager()
+        {
+            Console.WriteLine("this is CEO class  no parameter cnstructor");
+            this.perk = "jjdsj";
+             
+        }
+        public GeneralManager(String perk)
+        {
+            Console.WriteLine("this is general manager class  parameter cnstructor");
+            this.perk = perk;
+
+        }
+
+    }
+
+ public class CEO : Employee
+    {
+        private decimal basic;
+        public override decimal Basic
+        {
+
+            set
+            {   if (value > 1000)
                 {
-                    deptno = value;
+                    basic = value;
+                }
+            else
+                {
+                    Console.WriteLine("invalid entry of basic in CEO class");
+
                 }
             }
             get
             {
-                return Deptno;
+                return basic;
+
             }
+        }
+        public override  sealed decimal CalcNetSalary()
+        {
+            Console.WriteLine("this is calcNetsalary  ceo  class function");
+            int salary = (int)(basic + 250);
+            return salary;
+        }
+           public CEO()
+        {
+            Console.WriteLine("this is CEO class no parameter cnstructor");
+            this.basic = 1200;
+             
 
         }
 
-            public employee()
+        public CEO(int basic)
         {
-            this.empno = dd++;
-            
-
-
-        }
-
-        public employee(String name,decimal basic,short  deptno)
-        {
-            this.deptno = deptno;
-            this.name = name;
+            Console.WriteLine("this is CEO class  parameter cnstructor");
             this.basic = basic;
-            this.empno = dd++;
-        }
-        public employee(String name, decimal basic)
-        {
-            this.name = name;
-            this.basic = basic;
-            this.empno = dd++;
-        }
-        public employee(String name)
-        {
-            this.name = name;
-            this.empno = dd++;
-        }
 
-        public  int getnetsalary()
-        {
-            int netsalary = (int)basic + 1000;
-            return netsalary;
+
         }
     }
+
 }
-
-
