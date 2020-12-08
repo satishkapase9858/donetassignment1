ac4d9812dcbfe3d74402cccdc113ce8e76e06bea
satishkapase9858 <65151853+satishkapase9858@users.noreply.github.com>
2020-12-08 15:54:24 +0530

    assignment4Q1
    completed assignment2

--- Program.cs
+++ Program.cs
@@ -4,45 +4,65 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Assignment1
+namespace Assignment4
 {
     class Program
     {
         static void Main(string[] args)
         {
-            employee p1 = new employee();
+            employee[] e = new employee[3] ;
+            for (int i = 0; i <e.Length   ; i++) {
+
+                Console.WriteLine("enter the name of employee");
+                string name = Convert.ToString(Console.ReadLine());
+
+                Console.WriteLine("enter the basic of employee");
+                decimal basic = Convert.ToDecimal (Console.ReadLine());
+
+
+                Console.WriteLine("enter the department number of employee");
+                short deptno = Convert.ToSByte(Console.ReadLine());
+                 
+                e[i] = new employee(name,basic,deptno);
+                Console.WriteLine("+++++++++++++++++++++++++++++++++++++++++");
+               
+            }
            
-            p1.Name ="sss";
-            p1.Deptno = 9;
-            p1.Basic = 15;
-            Console.WriteLine(p1.EmpNo);
-            Console.WriteLine(p1.Name);
-            Console.WriteLine(p1.Basic);
-           
+            
+            for (int i=0;i<e.Length;i++)
+            {
+                for (int j=i+1;j<e.Length;j++) {
+                      if (e[i].Basic>e[j].Basic)
+                    {
+                        employee m = e[i];
+                        e[i] = e[j];
+                        e[j] = m;
+                        
+                        
+                    }
+                        }
+            }
+            Console.WriteLine("employee name of highest salary{0}:::=", e[e.Length-1].Name);
 
+            Console.WriteLine("=====================================================");
 
-               employee p2 = new employee();
-                Console.WriteLine(p2.EmpNo);
-                
+            Console.WriteLine("enter the emloyee number to find all detail of that number");
+            int k = Convert.ToInt32(Console.ReadLine());
+              for(int i=0;i<e.Length;i++)
+            {
+                if(e[i].EmpNo==k)
+                {
+                    Console.Write("employee name={0}:",e[i].Name);
+                    Console.Write("employee deptno={0}:", e[i].Deptno);
+                    Console.Write("employee basic={0}:", e[i].Basic);
+                    Console.Write("employee empno={0}:", e[i].EmpNo);
+                    Console.ReadLine();
+                }
 
-
-
-            employee p3 = new employee();
-            Console.WriteLine(p3.EmpNo);
-
-
-
-            employee e1 = new employee("satish", 12212, 10);
-            employee e2 = new employee("satish", 12212);
-            employee e3 = new employee("satish");
-            Console.WriteLine(e1.getnetsalary());
+            }
             Console.ReadLine();
-
         }
     }
-
-
-
     class employee
     {
 
@@ -53,10 +73,10 @@ namespace Assignment1
         {
             set
             {
-                if (value=="")
+                if (value == "")
                 {
                     Console.WriteLine("invallid entry in String name");
-                    
+
                 }
                 else
                 {
@@ -75,40 +95,44 @@ namespace Assignment1
 
         }
 
-        private static int dd =1;
-          
+        private static int dd = 1;
 
-        private  int empno;
-           public   int EmpNo
+
+        private int empno;
+        public int EmpNo
         {
-             
-              get
+            private set
             {
-                return  empno;
+                empno = value;
+            }
+            get
+            {
+                return empno;
 
 
             }
 
         }
-          
-       
+
+
 
 
         private decimal basic;
         public decimal Basic
         {
-            set { 
-            if(value>10 && value< 100)
+            set
             {
+                if (value > 10 && value < 10000)
+                {
                     basic = value;
-            }
+                }
                 else
                 {
 
-                    Console.WriteLine("not allowed values in decimal part");
+                    Console.WriteLine("not allowed values in basic part");
                 }
 
-           }
+            }
             get
             {
 
@@ -117,55 +141,53 @@ namespace Assignment1
         }
 
         private short deptno;
-             public  short Deptno
+        public short Deptno
         {
             set
             {
-                if(value>0)
+                if (value > 0)
                 {
                     deptno = value;
                 }
             }
             get
             {
-                return Deptno;
+                return deptno;
             }
 
         }
 
-            public employee()
+        public employee()
         {
-            this.empno = dd++;
-            
+            this.EmpNo = dd++;
+
 
 
         }
 
-        public employee(String name,decimal basic,short  deptno)
+        public employee(String name, decimal basic, short deptno)
         {
-            this.deptno = deptno;
-            this.name = name;
-            this.basic = basic;
-            this.empno = dd++;
+            this.Deptno = deptno;
+           this.Name = name;
+            this.Basic = basic;
+            this.EmpNo = dd++;
         }
         public employee(String name, decimal basic)
         {
-            this.name = name;
-            this.basic = basic;
-            this.empno = dd++;
+            this.Name= name;
+            this.Basic = basic;
+            this.EmpNo = dd++;
         }
         public employee(String name)
         {
-            this.name = name;
-            this.empno = dd++;
+            this.Name= name;
+            this.EmpNo = dd++;
         }
 
-        public  int getnetsalary()
+        public int getnetsalary()
         {
             int netsalary = (int)basic + 1000;
             return netsalary;
         }
     }
 }
-
-
